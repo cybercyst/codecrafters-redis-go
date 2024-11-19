17ffd40b6de47435d2e4c9bf98330d3b00985fc2
Forrest Loomis <cybercyst@gmail.com>
2024-11-18 21:32:42 -0500

    feat: send handshake (3/3)

--- internal/replica/master.go
+++ internal/replica/master.go
@@ -3,6 +3,7 @@ package replica
 import (
 	"bufio"
 	"fmt"
+	"log/slog"
 	"net"
 	"strings"
 
@@ -19,7 +20,21 @@ func (m *MasterClient) Close() {
 	m.conn.Close()
 }
 
-func (m *MasterClient) RequestAndResponse(req, resp string) error {
+func (m *MasterClient) SendMessageAndGetResponse(req string) (string, error) {
+	encodedReq := encoder.EncodeArrayBulkString(strings.Split(req, " "))
+	_, err := (m.conn).Write(encodedReq)
+	if err != nil {
+		return "", fmt.Errorf("error sending message %s: %w", req, err)
+	}
+
+	message, err := bufio.NewReader(m.conn).ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("error getting reply: %w", err)
+	}
+	return message, nil
+}
+
+func (m *MasterClient) SendMessageAndExpectResponse(req, resp string) error {
 	encodedReq := encoder.EncodeArrayBulkString(strings.Split(req, " "))
 	_, err := (m.conn).Write(encodedReq)
 	if err != nil {
@@ -47,19 +62,25 @@ func NewMasterClient(masterAddress string, masterPort, listeningPort int) (*Mast
 		conn:    conn,
 	}
 
-	if err = client.RequestAndResponse("PING", "PONG"); err != nil {
+	if err = client.SendMessageAndExpectResponse("PING", "PONG"); err != nil {
 		return nil, fmt.Errorf("error sending PING: %w", err)
 	}
 
 	msg := fmt.Sprintf("REPLCONF listening-port %d", listeningPort)
-	if err = client.RequestAndResponse(msg, "OK"); err != nil {
+	if err = client.SendMessageAndExpectResponse(msg, "OK"); err != nil {
 		return nil, fmt.Errorf("error sending %s: %w", msg, err)
 	}
 
 	msg = "REPLCONF capa psync2"
-	if err = client.RequestAndResponse(msg, "OK"); err != nil {
+	if err = client.SendMessageAndExpectResponse(msg, "OK"); err != nil {
 		return nil, fmt.Errorf("error sending %s: %w", msg, err)
 	}
+
+	resp, err := client.SendMessageAndGetResponse("PSYNC ? -1")
+	if err != nil {
+		return nil, fmt.Errorf("error sending PSYNC: %w", err)
+	}
+	slog.Info("PSYNC response", slog.String("resp", resp))
 
 	return client, nil
 }
